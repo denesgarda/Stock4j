f2e0750e40e45e54694bbc1d5066a380398897cc
Denes Garda <72540138+DenDen747@users.noreply.github.com>
2021-03-25 14:27:21 -0700

    Created AllTickers.txt

--- Stock4j/src/test/java/Test2.java
+++ Stock4j/src/test/java/Test2.java
@@ -5,7 +5,9 @@ import coin.Stock4j.data.statics.Market;
 import java.io.FileNotFoundException;
 
 public class Test2 {
-    public static void main(String[] args) {
-        
+    public static void main(String[] args) throws FileNotFoundException {
+        for(int i = 0; i < Market.getAllTickersNumber(); i++) {
+            System.out.println(Market.getAllStocks()[i].getTicker());
+        }
     }
 }
