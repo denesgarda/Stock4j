6a5ff3cd1aa0817c82f5fcf1a0de5b8d3adc2886
Denes Garda <DenDen747@users.noreply.github.com>
2021-03-25 20:17:32 -0700

    Updated

---
+++ Stock4j/src/main/java/coin/Stock4j/data/statics/TimeType.java
@@ -0,0 +1,11 @@
+package coin.Stock4j.data.statics;
+
+public class TimeType {
+    public static TimeType DATE = new TimeType();
+    public static TimeType TIME = new TimeType();
+    public static TimeType DATE_AND_TIME = new TimeType();
+
+    private TimeType() {
+
+    }
+}
